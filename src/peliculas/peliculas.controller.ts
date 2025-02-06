739e1e8d804da723f40d07433c1fb279c5f87500
ElenaRiojaGil <eriojita@gmail.com>
2025-02-06 11:57:02 +0100

    4-02-2025

--- src/peliculas/peliculas.controller.ts
+++ src/peliculas/peliculas.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { PeliculasService } from './peliculas.service';
+import { Pelicula } from 'src/entities/pelicula.entity';
 
 @Controller('peliculas')
-export class PeliculasController {}
+export class PeliculasController {
+    constructor(private readonly peliculaService: PeliculasService){}
+
+    @Post()
+    async createPelicula(@Body() peli: Partial<Pelicula>): Promise<Pelicula>{
+        return this.peliculaService.create(peli);
+    }
+}
--- src/peliculas/peliculas.service.ts
+++ src/peliculas/peliculas.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Pelicula } from 'src/entities/pelicula.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PeliculasService {}
+
+export class PeliculasService {
+
+    constructor(
+        @InjectRepository(Pelicula)
+        private peliculasRepository: Repository<Pelicula>,
+    ) {} 
+    async findAll():Promise<Pelicula[]>{
+        return this.peliculasRepository.find();
+    }
+    async findOne(id: number):Promise<Pelicula | null >{
+        return this.peliculasRepository.findOne({ where: { id}});
+    }
+    async create(data: Partial<Pelicula>): Promise<Pelicula>{
+        const newPeli = this.peliculasRepository.create(data);
+        return this.peliculasRepository.save(newPeli);
+}
+}
